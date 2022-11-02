6210d302ffa914351d01509b4b0bdd302f0ca758
Anton (Parano9I) <madoc1948@gmail.com>
2022-11-02 09:11:36 +0200

    Change CarResource

--- frontend/src/components/dropDown/childs/dropDownItem/dropDownItem.component.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from 'react';
-
-interface DropDownItemInterface {
-  children: ReactNode;
-}
-
-const dropDownItem: FC<DropDownItemInterface> = ({ children }) => {
-  return <div>{children}</div>;
-};
