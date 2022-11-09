a6171f4fdcc580f1a1d1ee46381d5b73d0b9b990
Anton (Parano9I) <madoc1948@gmail.com>
2022-11-09 10:23:01 +0200

    Replaced old useSearchParams hook on useMySearchParams in Filter component

--- frontend/src/components/filter/filter.component.tsx
+++ frontend/src/components/filter/filter.component.tsx
@@ -1,8 +1,7 @@
 import { FC, ReactElement, useState } from 'react';
 import { FilterContext } from './context';
 import { FilterInterface } from './types';
-import GeneratorSearchParams from '../../helpers/GeneratorSearchParams';
-import { useSearchParams } from 'react-router-dom';
+import { useMySearchParams } from '../../hooks/mySearchParamsHook';
 
 interface FilterProps {
   className: string;
@@ -10,7 +9,7 @@ interface FilterProps {
 }
 
 const Filter: FC<FilterProps> = ({ className, children }) => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useMySearchParams();
   const [filters, setFilters] = useState<FilterInterface[]>([]);
 
   const addFilter = (newFilterState: FilterInterface): void => {
@@ -27,16 +26,13 @@ const Filter: FC<FilterProps> = ({ className, children }) => {
   };
 
   const handleApplyFilters = () => {
-    const generatorParams = new GeneratorSearchParams();
+    const params = filters.reduce((acc, curr) => {
+      if (curr.values.length) {
+        return { ...acc, [curr.name]: curr.values.join(',') };
+      } else return acc;
+    }, {});
 
-    filters.forEach((filter) => {
-      if (filter.values.length) {
-        generatorParams.set(filter.name, filter.values);
-      }
-    });
-
-    const paramsStr = generatorParams.getStringSearchParams();
-    setSearchParams(paramsStr);
+    setSearchParams({ ...searchParams, ...params });
   };
 
   return (
